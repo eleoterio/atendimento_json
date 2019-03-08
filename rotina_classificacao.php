7c1ebb14e7f4386fcd36059720200b2bf1634764
Felipe Eleoterio <felipe.eleoterio@telecontrol.com.br>
2019-03-08 15:57:54 -0300

    ajuste rotina

--- rotina_classificacao.php
+++ rotina_classificacao.php
@@ -1,25 +1,39 @@
 <?php
 error_reporting( E_ALL );
-
-$json_file = file_get_contents("tickets.json");   
+$arquivo = "tickets.json";
+$json_file = file_get_contents($arquivo);   
 $json = json_decode($json_file, true);
 
-foreach($json as $ticket){
+foreach($json as $key => $ticket){
     $abertura = new DateTime($ticket['DateCreate']);
     $update = new DateTime($ticket['DateUpdate']);
     $interval = $abertura->diff($update);
-    if ($interval->days > 5) {
-        $alta = 33.33;
+    $pontos = 0;
+    if (count($ticket['Interactions']) <= 1) {
+        $pontos += 20;
     }
-    foreach($ticket['Interactions'] as $interacao) {
-        if (preg_match('Reclamação', $interacao['Subject'])) {        
-            echo'<pre>';
-            var_dump($interacao);
-            die;
-        }
+    if ($interval->days > 5) 
+    {
+        $pontos += 20;
+    }
+    if ( preg_match('/reclamação/', strtolower($ticket['Interactions'][0]['Subject']) ) || preg_match('/reclamação/', strtolower($ticket['Interactions'][0]['Message']) ) ) 
+    {        
+         $pontos += 20;
+    } 
+    if ( preg_match('/troca/', strtolower($ticket['Interactions'][0]['Subject']) ) || preg_match('/troca/', strtolower($ticket['Interactions'][0]['Message']) ) )           
+    {         
+        $pontos += 20;
+    }
+ if ( preg_match('/procon/', strtolower($ticket['Interactions'][0]['Message']) ) || preg_match('/reclame/', strtolower($ticket['Interactions'][0]['Message']) ) )           
+    {         
+        $pontos += 20;
+    }
+    $json[$key]['ponctuation'] = $pontos;
+    if ($pontos > 20) {
+        $json[$key]['priority'] = "Alta";
+    } else {
+        $json[$key]['priority'] = "Normal";
     }
 }
-echo'<pre>';
-var_dump($json[1]['Interactions'][0]['Subject']);
-var_dump(preg_match('/Reclamação/', $json[1]['Interactions'][0]['Subject']));
-die;
+$json_format = json_encode($json, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+file_put_contents($arquivo, $json_format);
